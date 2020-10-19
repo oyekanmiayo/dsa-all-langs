278aa88436b878e2d8c34c97fcef820f5cc7e0eb
Ayomide Oyekanmi <ayomideoyekanmi@gmail.com>
2020-10-19 21:55:48 +0100

    Add methods for List

---
+++ list/List.java
@@ -0,0 +1,36 @@
+class List<E> {
+    E[] entries;
+
+    List(){
+        // Using a random initial size of 16
+        entries = new E[16];
+    }
+
+    public void add(E element){
+
+    }
+
+    public void add(int n, E element){
+
+    }
+
+    public E remove(int n){
+
+    }
+
+    public E get(int n){
+
+    }
+
+    public void sort(){
+
+    }
+
+    public List<E> slice(int start, int end){
+
+    }
+
+    public void reverse(){
+        
+    }
+}
