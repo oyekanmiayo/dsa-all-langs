8b845ebee9d3cc851f63d938fcbea3cc769dd5f2
Chidi Williams <williamschidi1@gmail.com>
2020-10-28 01:55:19 +0100

    Add queue implementations for JS and Go

--- queue/go/queue.go
+++ queue/go/queue.go
@@ -7,26 +7,26 @@ type Queue struct {
 }
 
 // Enqueue adds an element to the back of the queue.
-func (q Queue) Enqueue(element string) {
+func (q Queue) Enqueue(element interface{}) {
 	q.linkedList.AddLast(element)
 }
 
 // Dequeue removes the element at the front of the queue
 // and returns it. It returns an error if the queue is empty.
-func (q Queue) Dequeue() (string, error) {
+func (q Queue) Dequeue() (interface{}, error) {
 	return q.linkedList.RemoveFirst()
 }
 
 // Peek returns the element at the front of the queue.
 // It returns an error if the queue is empty.
-func (q Queue) Peek() (string, error) {
+func (q Queue) Peek() (interface{}, error) {
 	return q.linkedList.Peek()
 }
 
 type doublyLinkedList interface {
-	AddLast(element string)
-	RemoveFirst() (string, error)
-	Peek() (string, error)
+	AddLast(element interface{})
+	RemoveFirst() (interface{}, error)
+	Peek() (interface{}, error)
 }
 
 // NewQueue returns a new Queue.
