935a020d4ecec63e1f6898ecb5a8b5e15531d3e1
Flappizy <75231665+Flappizy@users.noreply.github.com>
2021-04-23 03:24:20 +0100

    C# Implementation

---
+++ list/C#/List.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Data_Structure
+{
+    // @author : Babatunde Ibukun
+
+    class List<T> : IEnumerable<T>
+    {
+        private T[] array;
+        private int count;
+
+        public List()
+        {
+            this.array = new T[16];
+            this.count = 0;
+        }
+
+        public List(int capacity)
+        {
+            this.array = new T[capacity];
+            this.count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public void ExpandArrayIfFull()
+        {
+            if (this.Count + 1 > this.array.Length)
+            {
+                int newLength = this.array.Length * 2; 
+                T[] expandedArray = new T[newLength];
+
+                //What happens here is that, i am copying the old array(full one) into a new one that has an increased size, the last argument 
+                //specifies the amount of the old array i want to copy into the new, and i specified the whole length
+                Array.Copy(this.array, expandedArray, this.Count);
+                this.array = expandedArray;
+            }
+        }
+
+        public void Add(T item)
+        {
+            ExpandArrayIfFull();
+            this.array[this.Count] = item;
+            this.count++;
+        }
+
+        public void Add(int index, T item)
+        {
+            if (index > this.Count || index < 0)
+            {
+                throw new IndexOutOfRangeException($"The index {index} you specified is not within the range of the list, please specify " +
+                    $"another index");
+            }
+            ExpandArrayIfFull();
+            //What i am trying to do here is, i am moving the original element at the specified position where we want to put our new element
+            //one place to the right
+            Array.Copy(this.array, index, this.array, index + 1, this.count - index);
+            this.array[index] = item;
+            this.count++;
+        }
+
+        public void Clear()
+        {
+            this.array = new T[16];
+            this.count = 0;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.array.Length; i++)
+            {
+                if (item.Equals(this.array[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            int index = IndexOf(item);
+            bool found = (index != -1);
+            return found;
+        }
+
+        public T Get(int index)
+        {
+            if (index > this.count || index < 0)
+            {
+                throw new IndexOutOfRangeException($"{index} is invalid");
+            }
+           return this.array[index];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.Count || index < 0 )
+                {
+                    throw new ArgumentOutOfRangeException($"Index {index} is invalid");
+                }
+                return this.array[index];
+            }
+            set
+            {
+                if (index >= this.Count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"Index {index} is invalid");
+                }
+                this.array[index] = value;
+            }
+        }
+
+        public T Remove(int index)
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException($"Index {index} is invalid");
+            }
+            
+            T item = this.array[index];
+
+            //What i am trying do here is, i am moving every element on right side of the specified element we want to remove one place backward
+            //This allows me to overwrite the element i want to remove
+            Array.Copy(this.array, index + 1, this.array, index, this.count - index - 1);
+            //I assign a value (default of whatever type we working with) to the empty index after moving elements within the array
+            this.array[this.count - 1] = default(T);
+            this.count--;
+            return item;
+        }
+
+        public void Sort()
+        {
+            Array.Sort(this.array);
+        }
+
+        public List<T> Slice(int start, int end)
+        {
+            if (start >= this.count || start < 0)
+            {
+                throw new IndexOutOfRangeException($"{start} is an invalid index");
+            }
+            if (end >= this.count || end < 0)
+            {
+                throw new IndexOutOfRangeException($"{end} is an invalid index");
+            }
+            
+            List<T> slicedItems = new List<T>();
+             
+            for (int i = start; i <= end; i++)
+            {
+                slicedItems.Add(this.array[i]);
+            }
+            return slicedItems;
+        }
+
+        public void Reverse()
+        {
+            int start = 0;
+            int end = this.count - 1;
+            
+            for (int i = 0; i < this.count / 2; i++)
+            {
+                T temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in this.array)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(34);
+            numbers.Add(4);
+            numbers.Add(20);
+            numbers.Add(2, 28);
+            numbers.Add(3, 30);
+
+            Console.WriteLine(numbers.Get(4)); //20
+            Console.WriteLine(numbers.Remove(2));
+            List<int> sliced = numbers.Slice(1, 3); // 4, 28, 30
+            numbers.Sort(); // 4, 20, 28, 30, 34
+            numbers.Reverse(); //20, 30, 28, 4, 34
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ queue/C#/Queue.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Data_Structure
+{
+    // @author : Babatunde Ibukun
+    //@notes This implementation uses a Doubly Linked List as the underlying data structure
+
+    public class Queue<T>
+    {
+        private Node front;
+        private Node rear;
+        private int count;
+
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node NextNode { get; set; }
+
+            public Node(T element)
+            {
+                Element = element;
+                NextNode = null;
+            }
+
+            public Node(T element, Node previousNode)
+            {
+                Element = element;
+                previousNode.NextNode = this;
+            }
+        }
+
+        public Queue()
+        {
+            front = null;
+            rear = null;
+            count = 0;
+        }
+
+
+        public void Enqueue(T item)
+        {
+            //Before adding the element to the queue, i check if it is empty, if it is empty, i set the element to the front of the queue, i also
+            //make the element the rear of the queue, since we have only a single element within the queue
+            if (this.front == null)
+            {
+                this.front = new Node(item);
+                this.rear = this.front;
+            }
+
+            //If the queue is not empty then i add the new item to the end of the queue 
+            else
+            {
+                //I create the new node i want to add and i pass the item i want to add and the current rear as parameters to the node's constructor
+                Node newNode = new Node(item, this.rear);
+                this.rear = newNode;
+            }
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            //Checks if the queue i want to remove from is empty or not
+            if (this.count <= 0)
+            {
+                throw new InvalidOperationException("The Queue is empty");
+            }
+            
+            //I get the front i want to dequeue and i put it into a variable so that i can return it 
+            Node originalFront = this.front;
+
+            //The front's succeeding node is set as the new front
+            this.front = this.front.NextNode;
+            
+            //Checks if the new front is null, if it is, i make the queue empty
+            if (this.front == null)
+            {
+                this.rear = null;
+            }
+            count--;
+            return originalFront.Element;
+        }
+
+        public T Peek()
+        {
+            if (this.count <= 0)
+            {
+                throw new InvalidOperationException("The Queue is empty");
+            }
+            return this.front.Element;
+        }
+
+        private int IndexOf(T item)
+        {
+            int index = 0;
+            Node currentNode = this.front;
+            Node previousNode = null;
+
+            while (currentNode != null)
+            {
+                if (object.Equals(currentNode.Element, item))
+                {
+                    return index;
+                }
+                previousNode = currentNode;
+                currentNode = currentNode.NextNode;
+                index++;
+            }
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            int index = IndexOf(item);
+            bool found = (index != -1);
+            return found;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(10);
+            numbers.Enqueue(8);
+            numbers.Enqueue(12);
+            numbers.Enqueue(3);
+            Console.WriteLine(numbers.Dequeue());//10;
+            Console.WriteLine(numbers.Peek());//8;
+        }
+    }
+}
